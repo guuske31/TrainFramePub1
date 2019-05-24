895a22fc80145535380713c3dffcacc78547f593
guuske31 <50746212+guuske31@users.noreply.github.com>
2019-05-24 16:11:24 +0200

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -40,7 +40,7 @@ def image_preporcess(image, target_size, gt_boxes=None):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
   # image = (np.float32(image), cv2.COLOR_BGR2RGB)#.astype(np.float32)
 
-    print(image.shape)
+   # print(image.shape)
     ih, iw    = target_size
     h,  w, _  = image.shape
 
