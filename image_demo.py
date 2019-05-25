15f0694dc9b75121a5dd47e2539b969e5233e086
guuske31 <50746212+guuske31@users.noreply.github.com>
2019-05-25 15:03:18 +0200

    Update image_demo.py

--- image_demo.py
+++ image_demo.py
@@ -19,7 +19,7 @@ from PIL import Image
 
 return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
 pb_file         = "./yolov3_coco.pb"
-image_path      = "./docs/images/.jpg"
+image_path      = "./dataimages/Frame00022.jpg"
 num_classes     = 80
 input_size      = 416
 graph           = tf.Graph()
