2e13d156e740712ca8619ea40fddc671b89d141b
guuske31 <50746212+guuske31@users.noreply.github.com>
2019-06-04 12:10:19 +0200

    Update config.py

--- core/config.py
+++ core/config.py
@@ -39,7 +39,7 @@ __C.TRAIN                       = edict()
 __C.TRAIN.ANNOT_PATH            = "./data/dataset/traindataset.txt"
 __C.TRAIN.BATCH_SIZE            = 2
 __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
-__C.TRAIN.DATA_AUG              = True
+__C.TRAIN.DATA_AUG              = False
 __C.TRAIN.LEARN_RATE_INIT       = 1e-4
 __C.TRAIN.LEARN_RATE_END        = 1e-6
 __C.TRAIN.WARMUP_EPOCHS         = 5
@@ -56,9 +56,9 @@ __C.TEST.INPUT_SIZE             = 544
 __C.TEST.DATA_AUG               = False
 __C.TEST.WRITE_IMAGE            = True
 __C.TEST.WRITE_IMAGE_PATH       = "./detection/"
-__C.TEST.WRITE_IMAGE_SHOW_LABEL = False
+__C.TEST.WRITE_IMAGE_SHOW_LABEL = True
 __C.TEST.WEIGHT_FILE            = "./checkpoint/yolov3_test_loss=1.6982.ckpt-98"
-__C.TEST.SHOW_LABEL             = False
+__C.TEST.SHOW_LABEL             = True
 __C.TEST.SCORE_THRESHOLD        = 0.3
 __C.TEST.IOU_THRESHOLD          = 0.5
 
